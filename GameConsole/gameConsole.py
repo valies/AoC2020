c5a00260ce2c19c1ef7a72a4e8c6ea0f55361946
valies <da_valies@hotmail.com>
2020-12-08 13:06:14 +0100

    Day 8: GameConsole

---
+++ GameConsole/gameConsole.py
@@ -0,0 +1,99 @@
+class FileReader():
+
+    @classmethod
+    def read_file(cls, file):
+        a = []
+        with open(file) as my_file:
+            for line in my_file:
+                a.append(line.rstrip())
+        return a
+
+
+class Accumulator():
+
+    @classmethod
+    def form_instructions(cls, lines):
+        instructions = []
+
+        for i in range(len(lines)):
+            line = lines[i]
+            instruction = line.split(" ")[0]
+            move = int(line.split(" ")[1])
+            instruction = {"instruction": instruction, "move": move, "used": False}
+            instructions.append(instruction)
+        return instructions
+            
+
+    @classmethod
+    def process_part1(cls, instructions):
+        instruction_index = 0
+        used = False
+        accumulator = 0
+        while not used:
+            this_instruction = instructions[instruction_index]
+            move = this_instruction["move"]
+            instruction = this_instruction["instruction"]
+            used = this_instruction["used"]
+            if not used:
+                instructions[instruction_index]["used"] = True
+                if instruction == "acc":
+                    accumulator += move
+                    instruction_index += 1
+                if instruction == "jmp":
+                    instruction_index += move
+                if instruction == "nop":
+                    instruction_index += 1
+        return accumulator
+
+
+    @classmethod
+    def process_part2(cls, lines):
+        these_instructions = Accumulator.form_instructions(lines)
+        instruction_index = 0
+        accumulator = 0
+        turned_index = 0
+        turned_indexes = []
+        while True:
+            this_instruction = these_instructions[instruction_index]
+            move = this_instruction["move"]
+            instruction = this_instruction["instruction"]
+            used = this_instruction["used"]
+            if used:
+                these_instructions = Accumulator.form_instructions(lines)
+                instruction_index = 0
+                accumulator = 0
+                turned_index = 0
+                this_instruction = these_instructions[instruction_index]
+                move = this_instruction["move"]
+                instruction = this_instruction["instruction"]
+                used = this_instruction["used"]
+            if instruction_index == len(these_instructions)-1:
+                if instruction == "acc":
+                    accumulator += move
+                return accumulator
+            if turned_index == 0 and instruction in ("jmp", "nop") and instruction_index not in turned_indexes:
+                if instruction == "jmp":
+                    these_instructions[instruction_index]["instruction"] = "nop"
+                    instruction = "nop"
+                elif instruction == "nop" and abs(move) > 0:
+                    these_instructions[instruction_index]["instruction"] = "jmp"
+                    instruction = "jmp"
+                turned_index = instruction_index
+                turned_indexes.append(turned_index)
+            if not used:
+                these_instructions[instruction_index]["used"] = True
+                if instruction == "acc":
+                    accumulator += move
+                    instruction_index += 1
+                elif instruction == "jmp":
+                    instruction_index += move
+                elif instruction == "nop":
+                    instruction_index += 1
+        return "you should not be here"
+            
+
+if __name__ == '__main__':
+    lines = FileReader.read_file("accumulator/input.txt")
+    instructions = Accumulator.form_instructions(lines)
+    print(Accumulator.process_part1(instructions))
+    print(Accumulator.process_part2(lines))
---
+++ Tests/test_GameConsole.py
@@ -0,0 +1,32 @@
+import unittest
+from GameConsole.gameConsole import Accumulator
+
+
+class TestGameConsole(unittest.TestCase):
+
+    def test_accumulator_part1(self):
+        lines = ["nop +0",
+            "acc +1",
+            "jmp +4",
+            "acc +3",
+            "jmp -3",
+            "acc -99",
+            "acc +1",
+            "jmp -4",
+            "acc +6"]
+        instructions = Accumulator.form_instructions(lines)
+        result = Accumulator.process_part1(instructions)
+        self.assertEqual(result, 4, "Should be the same.")
+
+    def test_accumulator_part1(self):
+        lines = ["nop +0",
+            "acc +1",
+            "jmp +4",
+            "acc +3",
+            "jmp -3",
+            "acc -99",
+            "acc +1",
+            "jmp -4",
+            "acc +6"]
+        result = Accumulator.process_part2(lines)
+        self.assertEqual(result, 8, "Should be the same.")
